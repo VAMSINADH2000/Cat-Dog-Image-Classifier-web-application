ba3dc40bc1626f87c5b301549407805c76cd585d
KRISHNA VAMSI NADH <60399427+VAMSINADH2000@users.noreply.github.com>
2021-05-03 21:20:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,7 @@ from flask import Flask, render_template, request
 import numpy as np
 from tensorflow.keras.models import load_model
 import imageio
-import cv2
-
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
@@ -22,7 +19,7 @@ def make_prediction():
         img = imageio.imread(file)
         img_arr = np.array(img)
         img_size = 100
-        img_arr = cv2.resize(img_arr, (img_size, img_size))
+        img_arr.resize((img_size, img_size,img_arr.shape[2]))
         img_arr = np.array([img_arr])
         classifier = load_model('cat-dog-model.h5')
         prediction = classifier.predict_classes(img_arr)
