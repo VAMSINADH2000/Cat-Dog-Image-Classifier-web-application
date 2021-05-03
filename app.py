6363520515c95b4ef0f16cbc28a7cf81507434af
KRISHNA VAMSI NADH <60399427+VAMSINADH2000@users.noreply.github.com>
2021-05-03 20:25:03 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+import flask
+from flask import Flask, render_template, request
+import numpy as np
+from tensorflow.keras.models import load_model
+import imageio
+import cv2
+# import tensorflow as tf
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return flask.render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def make_prediction():
+    if request.method == 'POST':
+        file = request.files['image']
+        if not file:
+            return render_template('index.html', label="No file")
+        img = imageio.imread(file)
+        img_arr = np.array(img)
+        img_size = 100
+        img_arr = cv2.resize(img_arr, (img_size, img_size))
+        img_arr = np.array([img_arr])
+        classifier = load_model('cat-dog-model.h5')
+        prediction = classifier.predict_classes(img_arr)
+        label = int(prediction[0])
+        return render_template('index.html', label=label)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
