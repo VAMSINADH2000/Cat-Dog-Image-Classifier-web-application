4c9ac3a215e816842e7abc55669287570b704bb5
KRISHNA VAMSI NADH <60399427+VAMSINADH2000@users.noreply.github.com>
2021-05-03 20:34:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,11 @@ import numpy as np
 from tensorflow.keras.models import load_model
 import imageio
 import cv2
-# import tensorflow as tf
 
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/index')
 def index():
     return flask.render_template('index.html')
 
